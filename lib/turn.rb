826bcd08698dd69491345a65198cba66507c6540
Brian Haltiwanger <bhalt@bellsouth.net>
2018-11-20 18:11:17 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,43 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if position_taken?(board,index) == false && index.between?(0,8)
+      true
+  else
+      false
+  end
+end
+
+def position_taken? (board,index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  else board[index] == "X" || "O"
+    true
+  end
+end  
+
+def valid_move?(board, index,"X")
+  if position_taken?(board,index) == false && index.between?(0,8)
+      true
+  else
+      false
+  end
+end
+
+
+
+
+
+
+  
